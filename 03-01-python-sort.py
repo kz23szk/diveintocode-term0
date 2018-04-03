36133142f03ecd78020d3788509dc890fd65ce9a
suzuki kazufumi <skazufumi.a02525@gmail.com>
2018-04-03 19:23:14 +0900

    ソートする方法にラムダ式を導入

--- 03-01-python-sort.py
+++ 03-01-python-sort.py
@@ -11,23 +11,10 @@ def print_format(file_path):
         reader = csv.reader(csv_file)
         # 項目名は先に表示する
         print(' '.join(next(reader)))
-        address_number_list = []
-        print_str_list = []
-        # それぞれの行の郵便番号とスペース区切りの文字列をリストで格納する
-        for row_list in reader:
-            address_number_list.append(int(row_list[1]))
-            print_str_list.append(' '.join(row_list))
-
-        # 郵便番号順にソートする
-        for i in range(len(print_str_list)):
-            for j in range(i+1, len(print_str_list)):
-                if address_number_list[i] > address_number_list[j]:
-                    address_number_list[i], address_number_list[j] = address_number_list[j], address_number_list[i]
-                    print_str_list[i], print_str_list[j] = print_str_list[j], print_str_list[i]
-
-        # さいごにそれぞれの行を順番に表示する
-        for print_str in print_str_list:
-            print(print_str)
+        # 郵便番号順で並び替える
+        sorted_address = sorted(reader, key=lambda x: x[1]) 
+        for print_str in sorted_ddress:
+            print(' '.join(print_str))
 
 
 print_format('./address_data.csv')
