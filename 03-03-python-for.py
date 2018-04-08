023272f415ddb187afafdfec98ec3bb0c47fdcf6
h_tanouchi <skytraveler50@excite.co.jp>
2018-04-08 16:50:20 +0900

    ペアプロでのリファクタリング

--- 03-03-python-for.py
+++ 03-03-python-for.py
@@ -1,22 +1,22 @@
-#coding:utf-8
+# coding:utf-8
 WEEK_LIST = ['月', '火', '水', '木', '金', '土', '日']
-SUBJECT_LIST = ['Python', '数学', '機械学習', '深層学習','エンジニアプロジェクト']
+SUBJECT_LIST = ['Python', '数学', '機械学習', '深層学習', 'エンジニアプロジェクト']
 
 
 def output_schedule(study_time_list):
     '''今週の勉強予定を出力します'''
     study_subject_index = 0
+    len_SUBJECT_LIST = len(SUBJECT_LIST)
     for week_str, study_volume in zip(WEEK_LIST, study_time_list):
         if study_volume == 0:
             print("{}曜日は、お休みです。".format(week_str))
         else:
-            print("{0}曜日は{1}時間勉強する予定です。".format(week_str, study_volume))
+            print("{}曜日は、{}時間勉強する予定です。".format(week_str, study_volume))
             for limit in range(study_volume):
-                study_subject_index += 1
-                if study_subject_index == len(SUBJECT_LIST):
-                    study_subject_index = 0
-                print("{0}限目 {1}"
-                .format(str(limit+1),SUBJECT_LIST[study_subject_index]))
+                study_subject_index = (study_subject_index + 1) % len_SUBJECT_LIST
+                print("{}限目 {}"
+                      .format(limit+1, SUBJECT_LIST[study_subject_index]))
+
 
 def main():
     '''勉強情報をoutput_scheduleに渡します'''
